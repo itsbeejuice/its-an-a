4875c9880110a595cbdc59abd0b3a6f8a9106a89
Bee (Aka. Outlaw) <77043377+itsbeejuice@users.noreply.github.com>
2021-01-11 20:40:24 -0800

    Add files via upload

---
+++ house.py
@@ -0,0 +1,194 @@
+print("a cat\na house\nabird\n")
+start=input("What should I print?")
+
+
+if start=="a house":
+    print('''
+       *
+     * * *
+    * * * *
+  * * * * * *
+* * * * * * * *
+*             *
+*             *
+*             *
+*             *
+***************
+
+''' )
+
+else:
+    print("wrong, now enjoy your house you fool")
+    print('''
+       *
+     * * *
+    * * * *
+  * * * * * *
+* * * * * * * *
+*             *
+*             *
+*             *
+*             *
+***************
+
+''' )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #elliot if you're seeing this i dont fucking understand loops my guy but I could do the same thing with if, elif, and else and at this point everyone is trying to explain and i dont understanddddddd but it's okay because at this point I just gotta keep working and everything will be okay
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #also if you made it this far i really apreciate you and you are so fucking poggers i stg
