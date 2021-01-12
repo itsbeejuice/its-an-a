4495e5e8810159ee913c97ee8067da9f5ce29ef7
Bee (Aka. Outlaw) <77043377+itsbeejuice@users.noreply.github.com>
2021-01-11 20:35:41 -0800

    Add files via upload

---
+++ its an A.py
@@ -0,0 +1,15 @@
+print("a\nb\nc\n")
+
+'''
+     ***
+    *   *
+    *****
+    *   *
+    *   *
+    '''
+n=int(input("enter your rowwwwww "))
+m=int(input("enter your column "))
+print('why is this unescesarily difficult')
+for input1 in range (n):
+    for input2 in range (n):
+        if ((input==0 or input
